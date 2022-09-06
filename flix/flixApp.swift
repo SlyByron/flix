b96fd7570af115f1c130e22d186ef805b8136285
Byron <byroncooke@icloud.com>
2022-09-06 11:40:12 +0100

    chore: create project
    create xcode project template

---
+++ flix/flixApp.swift
@@ -0,0 +1,17 @@
+//
+//  flixApp.swift
+//  flix
+//
+//  Created by Byron on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct flixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
