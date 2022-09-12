16ea718d8261e1e07d1c753057daf95b243c6d8b
Byron <byroncooke@icloud.com>
2022-09-12 13:43:55 +0100

    chore: tmdb api Key
    store the movie database api key in code but with a comment saying why this is bad

--- Flix/Source/ViewModelFactory.swift
+++ Flix/Source/ViewModelFactory.swift
@@ -7,10 +7,15 @@
 
 import Foundation
 
+/* The API key should not be stored in code, normally this would be part of a build rule/script on CI
+ and entered manually during development with the use of an environment variable, not commited to the repo.
+ Having this here for the demo makes it easier to distribute and run but this is bad in a production app */
+private let tmdbAPIKey = "716b8b23cfb7a1b566eb35653fc539d9"
+
 /// Factory class responsible for constructing view models
 class ViewModelFactory: ObservableObject {
 
-  private let movieDBService = TheMovieDbService(apiKey: "716b8b23cfb7a1b566eb35653fc539d9")
+  private let movieDBService = TheMovieDbService(apiKey: tmdbAPIKey)
 
   /// Creates a view model for movie list
   /// - Returns: a fresh instance of a MovieListViewModel
